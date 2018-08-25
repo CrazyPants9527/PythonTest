80eae92d870eeff0f3b9697457227db2bf1b5c9d
CrazyPants9527 <291366458@Qqq.com>
2018-08-25 23:27:52 +0800

    习题7

---
+++ ex7.py
@@ -0,0 +1,18 @@
+print("Mary had a litter lamb.")
+print("Its fleece was white as %s"%"snow")
+print("And everywhere that Mary went.")
+print("."*10)
+s1='c'
+s2='h'
+s3='e'
+s4='e'
+s5='s'
+s6='e'
+s7='B'
+s8='u'
+s9='r'
+s10='g'
+s11='e'
+s12='r'
+print(s1+s2+s3+s4+s5+s6)
+print(s7+s8+s9+s10+s11+s12)
