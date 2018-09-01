58415bbd50eb1d74130ec969560dd9bb3abb9abb
CrazyPants9527 <291366458@Qqq.com>
2018-09-01 22:53:28 +0800

    习题答案

---
+++ ex34.py
@@ -0,0 +1,11 @@
+
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print(" Theanimal at 1.", animals[1])
+print("The 3rd animal.", animals[2])
+print("The 1st animal.", animals[0])
+print("The animal at 3.", animals[3])
+print("The 5th animal.", animals[4])
+print("The animal at 2.", animals[2])
+print("The 6th animal.", animals[5])
+print("The animal at 4", animals[4])
